8547173f751024d4ab09057571aa87e14b5b1cae
liuxiansheng123123 <18645080416@163.com>
2021-12-13 22:07:37 +0800

    Update InsertionSort.go

--- src/github.com/algorithm/InsertionSort.go
+++ src/github.com/algorithm/InsertionSort.go
@@ -4,31 +4,29 @@ import "fmt"
 
 //插入排序，类似于打扑克
 func insert(array []int, n int) {
-	//key := array[n]
-	//i := 0
-	//for {
-	//	if
-	//	array[n] = key
-	//		n--
-	//	i++
-	//	if !(array[n-1] < key) {
-	//		break
-	//	}
-	//}
-
+	key := array[n]
+	i := n
+	for array[i-1] > key {
+		array[i] = array[i-1]
+		i--
+		if i == 0 {
+			break
+		}
+	}
+	array[i] = key
 }
 
-func insertSort(array []int, n int) {
+//插入排序
+func insertionSort(array []int, n int) {
 	for i := 1; i < n; i++ {
-
+		insert(array, i)
 	}
-
 }
 
 //插入排序
 func main() {
-	selectionArray := []int{0, 8, 6, 4, 2, 3, 1, 9, 5, 7} //需要排序的数组
-	fmt.Println(selectionArray)                           //打印排序前数组
-	selectionSort(selectionArray)                         //排序
-	fmt.Println(selectionArray)                           //打印排序后数组
+	insertionArray := []int{0, 8, 6, 4, 2, 3, 1, 9, 5, 7} //需要排序的数组
+	fmt.Println(insertionArray)                           //打印排序前数组
+	insertionSort(insertionArray, len(insertionArray))    //排序
+	fmt.Println(insertionArray)                           //打印排序后数组
 }
